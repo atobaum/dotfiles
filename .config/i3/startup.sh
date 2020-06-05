9934a75ef279532fb8f1c72f1d2e9576333b0888
atobaum <atobaum@gmail.com>
2020-06-05 18:00:12 +0900

    fix xidlehook run script

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -13,19 +13,19 @@ xinput --set-prop 11 "Coordinate Transformation Matrix" 1.7 0 0 0 1.7 0 0 0 1 &
 if [ $(xrandr | grep -o "HDMI1") == "HDMI1" ]; then
 	xrandr --output HDMI1 --mode 1920x1080 --primary
 	xrandr --output eDP1 --off
-	export PRIMARY_DISPLAY=HDMI1
-else
-	export PRIMARY_DISPLAY=eDP1
 fi
 
 compton -b
 
+export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
+rm /tmp/xidlehook.sock
 xidlehook \
   --not-when-fullscreen \
   --not-when-audio \
-  --timer 600 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness .5' \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+  --timer 900 \
+    "xrandr --output $PRIMARY_DISPLAY --brightness .5" \
+    "xrandr --output $PRIMARY_DISPLAY --brightness 1" \
   --timer 300 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1 && systemctl suspend' \
-    ''
+    "xrandr --output $PRIMARY_DISPLAY --brightness 1 && systemctl suspend" \
+    '' \
+  --socket "/tmp/xidlehook.sock"
