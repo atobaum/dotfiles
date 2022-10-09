029d261c889fe4dda6841b3cc8bb62e73a9a5b09
atobaum <atobaum@gmail.com>
2022-10-10 00:04:16 +0900

    add neovim key maps
    - <S-h>, <S-l> for buffer switch
    - Arrow keys for pane resizing

---
+++ private_dot_config/nvim/lua/core/keymapping.lua
@@ -0,0 +1,9 @@
+-- Switch buffer
+vim.keymap.set('n', '<S-h>', ':bprevious<CR>')
+vim.keymap.set('n', '<S-l>', ':bnext<CR>')
+
+-- Resizing panes
+vim.keymap.set('n', '<Left>', ':vertical resize -1<CR>')
+vim.keymap.set('n', '<Right>', ':vertical resize +1<CR>')
+vim.keymap.set('n', '<Up>', ':resize -1<CR>')
+vim.keymap.set('n', '<Down>', ':resize +1<CR>')
