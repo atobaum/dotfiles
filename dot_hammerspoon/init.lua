c184d11fe68d543b6dd3908a37a6761e79682b62
atobaum <atobaum@gmail.com>
2022-11-26 20:48:49 +0900

    update hammerspoon

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,18 +1,34 @@
+function centerMouse()
+  local screen = hs.window.frontmostWindow():frame()
+  hs.alert.show(screen)
+  local pt = hs.geometry.rectMidPoint(screen)
+  hs.alert.show(pt)
+  hs.mouse.absolutePosition(pt)
+end
+
+function toggleApp(name)
+  return function()
+    local activated = hs.application.frontmostApplication()
+    local path = string.lower(activated:path())
+
+    if string.match(path, string.lower(name) .. '%.app$') then
+      activated:hide()
+      return
+    end
+
+    hs.application.launchOrFocus(name)
+  end
+end
+
 local f13_mode = hs.hotkey.modal.new()
 
-f13_mode:bind({}, "space", function()
-	hs.application.launchOrFocus("WezTerm")
-end)
-
-f13_mode:bind({}, "c", function()
-	hs.application.launchOrFocus("Google Chrome")
-end)
-
-f13_mode:bind({}, "o", function()
-	hs.application.launchOrFocus("Obsidian")
-end)
+f13_mode:bind({}, "space", toggleApp("WezTerm"))
+f13_mode:bind({}, "b", toggleApp("Google Chrome"))
+f13_mode:bind({}, "o", toggleApp("Obsidian"))
 
 hs.hotkey.bind({}, "f13",
 	function() f13_mode:enter() end,
 	function() f13_mode:exit() end
 )
+
+hs.alert.show("hammerspoon loaded")
