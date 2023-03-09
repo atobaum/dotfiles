7d1d266a5b09219a7a37fb4070d7372081b9d3b7
atobaum <atobaum@gmail.com>
2023-03-09 09:45:47 +0900

    add app shortcut

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -23,7 +23,8 @@ local f13_mode = hs.hotkey.modal.new()
 
 f13_mode:bind({}, "space", toggleApp("WezTerm"))
 f13_mode:bind({}, "b", toggleApp("Google Chrome"))
-f13_mode:bind({}, "o", toggleApp("Obsidian"))
+f13_mode:bind({}, "n", toggleApp("Obsidian"))
+f13_mode:bind({}, "v", toggleApp("Visual Studio Code"))
 
 hs.hotkey.bind({}, "f13",
 	function() f13_mode:enter() end,
