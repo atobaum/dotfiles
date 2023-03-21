30c18d1a8fcd5cc6535d1943c5183d1f6d213d17
atobaum <atobaum@gmail.com>
2023-03-21 14:26:13 +0900

    feat: update neovim config
    - changed vim script to lua
    - changed plugin manager: Plug -> lazy

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -3,3 +3,5 @@ vim.keymap.set('n', 'ff', builtin.find_files, {})
 vim.keymap.set('n', 'fg', builtin.live_grep, {})
 vim.keymap.set('n', 'fb', builtin.buffers, {})
 vim.keymap.set('n', 'fh', builtin.help_tags, {})
+
+require('telescope').setup({defaults = { file_ignore_patterns = {'node_modules'} }})
