ac49e2f6252351794df7a4dea3333da48d4aa55f
atobaum <atobaum@gmail.com>
2024-02-29 10:20:50 +0900

    feat(neovim): disable plugins in vscode context
    ref. https://marketplace.visualstudio.com/items?itemName=asvetliakov.vscode-neovim

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -12,107 +12,135 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  {
-    -- https://github.com/folke/tokyonight.nvim
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "day",
-      transparent = true
+if vim.g.vscode then
+  local plugins= {
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    {
+      'numToStr/Comment.nvim',
+      opts = {
+          -- add any options here
+      },
+      lazy = false,
     },
-  },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      -- https://github.com/folke/which-key.nvim
+    {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
     }
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-      { "<Leader>nt", ":NvimTreeToggle<CR>", desc = "ToggleNeoTree"}
-    },
-    config = function()
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        require("nvim-tree").setup {}
-    end,
-  },
-  {
-    -- synctac highlighting
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-  },
-  {
-    -- file search
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-  {
-    'numToStr/Comment.nvim',
-    opts = {
-        -- add any options here
-    },
-    lazy = false,
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup()
-    end
-  },
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-  },
-  {
-    "tpope/vim-fugitive",
-    event = "VeryLazy"
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "VeryLazy",
-    config = function()
-       require('gitsigns').setup({
-         signs = {
-           add = {
-             hl = 'GitSignsAdd',
-             text = '+',
-             numhl='GitSignsAddNr',
-             linehl='GitSignsAddLn'
-           },
-         }
-       })
-    end
   }
-}
 
-require("lazy").setup(plugins)
+  require("lazy").setup(plugins)
 
-require("plugins/nvim-treesitter")
-require("plugins/telescope")
-require("plugins/indent-blankline")
+  require("plugins/indent-blankline")
+else
+  local plugins = {
+    {
+      -- https://github.com/folke/tokyonight.nvim
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {
+        style = "day",
+        transparent = true
+      },
+    },
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+      end,
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        -- https://github.com/folke/which-key.nvim
+      }
+    },
+    {
+      "nvim-tree/nvim-tree.lua",
+      version = "*",
+      dependencies = {
+          "nvim-tree/nvim-web-devicons",
+      },
+      keys = {
+        { "<Leader>nt", ":NvimTreeToggle<CR>", desc = "ToggleNeoTree"}
+      },
+      config = function()
+          vim.g.loaded_netrw = 1
+          vim.g.loaded_netrwPlugin = 1
+          require("nvim-tree").setup {}
+      end,
+    },
+    {
+      -- synctac highlighting
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate"
+    },
+    {
+      -- file search
+      'nvim-telescope/telescope.nvim',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    {
+      'numToStr/Comment.nvim',
+      opts = {
+          -- add any options here
+      },
+      lazy = false,
+    },
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function()
+        require('lualine').setup()
+      end
+    },
+    {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+    },
+    {
+      "tpope/vim-fugitive",
+      event = "VeryLazy"
+    },
+    {
+      "lewis6991/gitsigns.nvim",
+      event = "VeryLazy",
+      config = function()
+         require('gitsigns').setup({
+           signs = {
+             add = {
+               hl = 'GitSignsAdd',
+               text = '+',
+               numhl='GitSignsAddNr',
+               linehl='GitSignsAddLn'
+             },
+           }
+         })
+      end
+    }
+  }
 
-vim.cmd[[colorscheme tokyonight]]
+  require("lazy").setup(plugins)
+
+  require("plugins/nvim-treesitter")
+  require("plugins/telescope")
+  require("plugins/indent-blankline")
+
+  vim.cmd[[colorscheme tokyonight]]
+end
+
