dfd88f29e4cff259e27649945e0d240dd936d883
atobaum <atobaum@gmail.com>
2024-10-28 14:50:00 +0900

    feat: add wezterm config

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,135 @@
+local wezterm = require 'wezterm'
+local appearnce = require 'appearance'
+
+local config = wezterm.config_builder()
+
+-- -- Use it!
+-- if appearance.is_dark() then
+--   config.color_scheme = 'Tokyo Night'
+-- else
+--   config.color_scheme = 'Tokyo Night Day'
+-- end
+
+config.color_scheme = 'Tokyo Night'
+
+config.font = wezterm.font({ family = 'JetBrains Mono' })
+config.font_size = 13
+
+-- Slightly transparent and blurred background
+config.window_background_opacity = 0.92
+config.macos_window_background_blur = 30
+-- Removes the title bar, leaving only the tab bar. Keeps
+-- the ability to resize by dragging the window's edges.
+-- On macOS, 'RESIZE|INTEGRATED_BUTTONS' also looks nice if
+-- you want to keep the window controls visible and integrate
+-- them into the tab bar.
+config.window_decorations = 'INTEGRATED_BUTTONS'
+-- Sets the font for the window frame (tab bar)
+config.window_frame = {
+  -- Berkeley Mono for me again, though an idea could be to try a
+  -- serif font here instead of monospace for a nicer look?
+  font = wezterm.font({ family = 'JetBrains Mono', weight = 'Bold' }),
+  font_size = 11,
+}
+
+wezterm.on('update-status', function(window)
+  -- Grab the utf8 character for the "powerline" left facing
+  -- solid arrow.
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+  -- Grab the current window's configuration, and from it the
+  -- palette (this is the combination of your chosen colour scheme
+  -- including any overrides).
+  local color_scheme = window:effective_config().resolved_palette
+  local bg = color_scheme.background
+  local fg = color_scheme.foreground
+
+  window:set_right_status(wezterm.format({
+    -- First, we draw the arrow...
+    { Background = { Color = 'none' } },
+    { Foreground = { Color = bg } },
+    { Text = SOLID_LEFT_ARROW },
+    -- Then we draw our text
+    { Background = { Color = bg } },
+    { Foreground = { Color = fg } },
+    { Text = ' ' .. wezterm.strftime('%a %b %-d %H:%M') .. ' ' },
+  }))
+end
+)
+
+
+config.set_environment_variables = {
+  PATH = '/opt/homebrew/bin:' .. os.getenv('PATH')
+}
+
+local function move_pane(key, direction)
+  return {
+    key = key,
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection(direction),
+  }
+end
+
+local function resize_pane(key, direction)
+  return {
+    key = key,
+    action = wezterm.action.AdjustPaneSize { direction, 3 }
+  }
+end
+
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+  {
+    key = 'a',
+    -- When we're in leader mode _and_ CTRL + A is pressed...
+    mods = 'LEADER|CTRL',
+    -- Actually send CTRL + A key to the terminal
+    action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
+  },
+  move_pane('j', 'Down'),
+  move_pane('k', 'Up'),
+  move_pane('h', 'Left'),
+  move_pane('l', 'Right'),
+  {
+    key = ',',
+    mods = 'SUPER',
+    action = wezterm.action.SpawnCommandInNewTab {
+      cwd = wezterm.home_dir,
+      args = { 'nvim', wezterm.config_file },
+    },
+  },
+  {
+    key = '"',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = ':',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'r',
+    mods = 'LEADER',
+    -- Activate the `resize_panes` keytable
+    action = wezterm.action.ActivateKeyTable {
+      name = 'resize_panes',
+      -- Ensures the keytable stays active after it handles its
+      -- first keypress.
+      one_shot = false,
+      -- Deactivate the keytable after a timeout.
+      timeout_milliseconds = 1000,
+    }
+  },
+}
+
+config.key_tables = {
+  resize_panes = {
+    resize_pane('j', 'Down'),
+    resize_pane('k', 'Up'),
+    resize_pane('h', 'Left'),
+    resize_pane('l', 'Right'),
+  },
+}
+
+return config
