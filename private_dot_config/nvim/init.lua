6b9b57943cf201d52cda5017d2531e837f037ed3
atobaum <atobaum@gmail.com>
2026-04-08 12:46:23 +0900

    fix(nvim): workaround Neovim 0.12 Shift+digit bug in VSCode

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,3 +1,18 @@
+-- Workaround: Neovim 0.12 + VSCode terminal Shift+digit bug
+-- https://github.com/neovim/neovim/issues/38651
+if vim.env.TERM_PROGRAM == 'vscode' then
+  local shifted_digits = {
+    ['<S-1>'] = '!', ['<S-2>'] = '@', ['<S-3>'] = '#',
+    ['<S-4>'] = '$', ['<S-5>'] = '%', ['<S-6>'] = '^',
+    ['<S-7>'] = '&', ['<S-8>'] = '*', ['<S-9>'] = '(',
+    ['<S-0>'] = ')',
+  }
+  for lhs, rhs in pairs(shifted_digits) do
+    vim.keymap.set({ 'n', 'x', 'i' }, lhs, rhs, { noremap = true, silent = true })
+    vim.keymap.set('c', lhs, function() return rhs end, { expr = true, noremap = true })
+  end
+end
+
 -- set up options
 vim.opt.langmenu = 'en_US.UTF-8'
 vim.opt.enc = 'utf8'
